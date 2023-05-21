7a8045e0c0a3bfa737e96d4afa38ef6794b51ce3
sourabhadap <adapsourabh@gmail.com>
2023-05-21 21:13:36 +0530

    Added bigger numbers case with tests

--- main.py
+++ main.py
@@ -17,14 +17,20 @@ def check_negative_numbers(numbers: List[str]):
         raise Exception(f'Negatives not allowed: {negative_numbers}')
 
 
+def check_bigger_numbers(numbers: List[str]):
+    numbers = [0 if int(n) > 1000 else int(n) for n in numbers]
+    return numbers
+
+
 def add(numbers: str) -> int:
     if numbers == '':
         return 0
     numbers_list = split_numbers(numbers)
     check_negative_numbers(numbers_list)
+    numbers_list = check_bigger_numbers(numbers_list)
     print(sum(map(int, numbers_list)))
     return sum(map(int, numbers_list))
 
 
 if __name__ == '__main__':
-    add('//;\n1;2;4;5;7')
+    add('//***\n1***1001***3')
--- tests.py
+++ tests.py
@@ -21,12 +21,27 @@ def test_add_new_line_delimiter():
     assert add("1\n2,3") == 6
 
 
-def test_negative_numbers():
+def test_negative_numbers_without_delimiter():
     try:
         add("-1,2,-3")
     except Exception as e:
         assert str(e) == "Negatives not allowed: [-1, -3]"
 
 
+def test_negative_numbers_with_delimiter():
+    try:
+        add("//;\n-1;2;-3")
+    except Exception as e:
+        assert str(e) == "Negatives not allowed: [-1, -3]"
+
+
 def test_add_custom_delimiter():
     assert add("//;\n1;2;4;5;7") == 19
+
+
+def test_add_bigger_numbers_without_delimiter():
+    assert add("2,1001,6") == 8
+
+
+def test_add_bigger_numbers_with_delimiter():
+    assert add("//;\n2;1001;6") == 8
